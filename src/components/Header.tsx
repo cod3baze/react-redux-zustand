30d5faadd90ac2c3daa42412dfb8898e409d1e0a
elias.alexandre <elliasallex@gmail.com>
2023-10-03 13:42:56 -0300

    dispatch current lesson

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,19 @@
+import { useAppSelector } from "../store";
+
 export function Header() {
+  const lesson = useAppSelector((state) => {
+    const { currentLessonIndex, currentModuleIndex } = state.player;
+
+    const currentModule = state.player.course.modules[currentModuleIndex];
+    const currentLesson = currentModule.lessons[currentLessonIndex];
+
+    return { currentModule, currentLesson };
+  });
+
   return (
     <div className="flex flex-col gap-1">
-      <h1 className="text-2xl font-bold">Fundamentos do Redux</h1>
-      <span>Módulo "Desvendando o Redux"</span>
+      <h1 className="text-2xl font-bold">{lesson.currentLesson.title}</h1>
+      <span>Módulo "{lesson.currentModule.title}"</span>
     </div>
   );
 }
