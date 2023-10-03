c8bf3f5cea0cc7f99eb252644672fbeb0c6415de
elias.alexandre <elliasallex@gmail.com>
2023-10-03 14:06:49 -0300

    config automatic auto-play

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,7 +1,12 @@
 import Player from "react-player/youtube";
+import { useDispatch } from "react-redux";
+
 import { useAppSelector } from "../store";
+import { next } from "../store/slices/player";
 
 export function Video() {
+  const dispatch = useDispatch();
+
   const lesson = useAppSelector((state) => {
     const { currentLessonIndex, currentModuleIndex } = state.player;
 
@@ -13,13 +18,19 @@ export function Video() {
     return currentLesson;
   });
 
+  function handlePlayNext() {
+    dispatch(next());
+  }
+
   return (
     <div className="w-full bg-zinc-950 aspect-video">
       <Player
         width="100%"
         height="100%"
         controls
+        playing
         url={`https://www.youtube.com/watch?v=${lesson.id}`}
+        onEnded={handlePlayNext}
       />
     </div>
   );
